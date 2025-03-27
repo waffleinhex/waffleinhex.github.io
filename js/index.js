2ada870856a9a2aad656e1e0a3382ee256a4eefc
waffleinhex <waffleinhex@gmail.com>
2025-03-27 14:02:32 -0600

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 //This could have been made better
 
-var projects = ["chckSum", "textReveal"];
-var projectsUrl = ["https://github.com/waffleinhex/chckSum/", "https://waffleinhex.github.io/textreveal/"];
+var projects = ["chckSum", "textReveal", "chrombeookgames"];
+var projectsUrl = ["https://github.com/waffleinhex/chckSum/", "https://waffleinhex.github.io/textreveal/", "https://waffleinhex.github.io/chromebookgames"];
 
 setInterval(function() {
     var rand = Math.floor(Math.random()*projects.length)
